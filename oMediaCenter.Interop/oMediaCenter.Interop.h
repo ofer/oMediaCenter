d5358c023f59c88b06d2ff153eaf137593edbf4d
oachler <ofer@achler.org>
2016-09-24 09:13:28 +0300

    Add project files.

---
+++ oMediaCenter.Interop/oMediaCenter.Interop.h
@@ -0,0 +1,13 @@
+// oMediaCenter.Interop.h
+
+#pragma once
+
+using namespace System;
+
+namespace oMediaCenterInterop {
+
+	public ref class Class1
+	{
+		// TODO: Add your methods for this class here.
+	};
+}
