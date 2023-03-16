c7736aae78867d72c6798bfe4427746d0b05ea3e
Ofer <ofer.achler@gmail.com>
2023-03-16 20:22:12 +0000

    Made the media file list order the shows by name, year, season, episode

--- oMediaCenter.Web/wwwsource/app/media-list/media-list.component.ts
+++ oMediaCenter.Web/wwwsource/app/media-list/media-list.component.ts
@@ -13,17 +13,35 @@ export class MediaListComponent implements OnInit {
   mediaFileList!: MediaFileRecord[];
 
   constructor(
-      private mediaDataService: MediaDataService,
-      private router: Router
+    private mediaDataService: MediaDataService,
+    private router: Router
   ) { }
 
   ngOnInit(): void {
-      this.mediaDataService.getMediaFileRecords()
-          .then(fileRecords =>
-              this.mediaFileList = fileRecords);
+    this.mediaDataService.getMediaFileRecords()
+      .then(fileRecords =>
+        this.mediaFileList = this.sort(fileRecords));
+  }
+
+  sort(fileRecords: MediaFileRecord[]): MediaFileRecord[] {
+    return fileRecords.sort((mfra, mfrb) => {
+      let nameComparison = mfra.name.localeCompare(mfrb.name);
+      if (nameComparison == 0) {
+        if (mfra.year != mfrb.year)
+          return mfra.year > mfrb.year ? 1 : -1;
+        if (mfra.season != mfrb.season)
+          return mfra.season > mfrb.season ? 1 : -1;
+        if (mfra.episode != mfrb.episode)
+          return mfra.episode > mfrb.episode ? 1 : -1;
+      } else {
+        return nameComparison;
+      }
+
+      return 0;
+    });
   }
 
   onSelect(media: MediaFileRecord) {
-      this.router.navigate(['/media', media.hash]);
+    this.router.navigate(['/media', media.hash]);
   }
 }
