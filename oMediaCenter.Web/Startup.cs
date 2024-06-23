85db7d3f2eacc6dc3545dd0da59f5f103d5cfc84
Ofer Achler <oachler@xsightsys.com>
2024-06-23 22:33:49 +0300

    Updated to fix obsolete interface usage

--- oMediaCenter.Web/Startup.cs
+++ oMediaCenter.Web/Startup.cs
@@ -14,7 +14,7 @@ namespace oMediaCenter.Web
 {
   public class Startup
   {
-    public Startup(IHostingEnvironment env)
+    public Startup(IWebHostEnvironment env)
     {
       var builder = new ConfigurationBuilder()
           .SetBasePath(env.ContentRootPath)
@@ -69,7 +69,7 @@ namespace oMediaCenter.Web
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline
-    public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
     {
       //loggerFactory.AddConsole(Configuration.GetSection("Logging"));
       //loggerFactory.AddDebug();
